0aa67b4be433f8210cce6ac28f2882ea8128954f
Ertugrul Sencer <48177057+ertugrulsencer@users.noreply.github.com>
2021-03-09 00:14:51 +0300

    Image width problem Updated

--- easey-loader.js
+++ easey-loader.js
@@ -1,77 +1,98 @@
 class EasyLoaderImage extends HTMLElement {
-	constructor() {
-		super();
-		this.template = `<img alt="test" src=""/>`;
-		this.attachShadow({
-			mode: 'open'
-		});
-		this.element = document.createElement('template');
-		this.element.innerHTML = this.template;
-		this.shadowRoot.appendChild(this.element.content.cloneNode(true));
-	}
+  constructor() {
+    super();
+    this.template = `<img alt="test" src=""/>`;
+    this.attachShadow({
+      mode: "open",
+    });
+    this.element = document.createElement("template");
+    this.element.innerHTML = this.template;
+    this.shadowRoot.appendChild(this.element.content.cloneNode(true));
+  }
 
-	connectedCallback() {
-		const imgElement = this.shadowRoot.querySelector('img')
-		let imageSrc = this.getAttribute('src');
-		let imageAlt = this.getAttribute('alt');
-		let imageAnimation = this.getAttribute('easy-animation');
+  connectedCallback() {
+    const imgElement = this.shadowRoot.querySelector("img");
+    let imageSrc = this.getAttribute("src");
+    let imageAlt = this.getAttribute("alt");
+    let imageWidth = this.getAttribute("width");
+    let imageAnimation = this.getAttribute("easy-animation");
 
-		function getAnimate(element) {
-			switch (imageAnimation) {
-				case 'fade':
-					element.animate([{
-							'opacity': '0'
-						},
-						{
-							'opacity': '1'
-						}
-					], {
-						duration: 750,
-						easing: 'linear'
-					})
-					break;
-				case 'scale':
-					element.animate([{
-							transform: 'scale(0)'
-						},
-						{
-							transform: 'scale(1)'
-						}
-					], {
-						duration: 500,
-						easing: 'ease'
-					})
-					break;
-			}
-		}
+    function getAnimate(element) {
+      switch (imageAnimation) {
+        case "fade":
+          element.animate(
+            [
+              {
+                opacity: "0",
+              },
+              {
+                opacity: "1",
+              },
+            ],
+            {
+              duration: 750,
+              easing: "linear",
+            }
+          );
+          break;
+        case "scale":
+          element.animate(
+            [
+              {
+                transform: "scale(0)",
+              },
+              {
+                transform: "scale(1)",
+              },
+            ],
+            {
+              duration: 500,
+              easing: "ease",
+            }
+          );
+          break;
+      }
+    }
 
-		function observerCallback(entries) {
-			entries.forEach(entrie => {
-				console.log(imageSrc);
-				if (entrie.isIntersecting) {
-					entrie.target.src = imageSrc;
-					getAnimate(entrie.target);
-					observer.disconnect();
-					if (imageSrc != undefined && imageSrc != null && imageSrc.trim() != '') {
-						imgElement.src = imageSrc.trim()
-						if (imageAlt != undefined && imageAlt != null && imageAlt.trim() != '') {
-							imgElement.alt = imageAlt
-						} else {
-							imgElement.alt = 'Easy Loader Error'
-						}
-					} else {
-						imgElement.alt = 'Easy Loader Error'
-					}
-				}
-			})
-		}
+    function observerCallback(entries) {
+      entries.forEach((entrie) => {
+        console.log(imageSrc);
+        if (entrie.isIntersecting) {
+          entrie.target.src = imageSrc;
+          getAnimate(entrie.target);
+          observer.disconnect();
+          imgElement.style.width = imageWidth;
+          console.log(imageWidth);
+          if (
+            imageSrc != undefined &&
+            imageSrc != null &&
+            imageSrc.trim() != ""
+          ) {
+            imgElement.src = imageSrc.trim();
+            if (
+              imageAlt != undefined &&
+              imageAlt != null &&
+              imageAlt.trim() != ""
+            ) {
+              imgElement.alt = imageAlt;
+            } else {
+              imgElement.alt = "Easy Loader Error";
+            }
+          } else {
+            imgElement.alt = "Easy Loader Error";
+          }
+        }
+      });
+    }
 
-		const observer = new IntersectionObserver(observerCallback, {
-			rootMargin: '10px'
-		})
+    const observer = new IntersectionObserver(observerCallback, {
+      rootMargin: "10px",
+    });
 
-		observer.observe(imgElement)
-	}
+    observer.observe(imgElement);
+  }
 }
 
-window.addEventListener('load', () => window.customElements.define('easy-img', EasyLoaderImage))
+window.addEventListener("load", () =>
+  window.customElements.define("easy-img", EasyLoaderImage)
+);
